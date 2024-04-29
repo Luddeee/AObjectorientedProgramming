cbaa93d608947ca24fd9c92b40edbbe0a322464c
Luddeee <71329848+Luddeee@users.noreply.github.com>
2024-04-29 11:44:20 +0200

    Updated saver

--- Lab3/Saver.java
+++ Lab3/Saver.java
@@ -8,7 +8,7 @@ public class Saver {
         return save(o, 0);
     }
 
-    private String save(Object o, int depth) throws IllegalAccessException, InvocationTargetException {
+    private String save(Object o, int jumps) throws IllegalAccessException, InvocationTargetException {
         if (o == null) return "";
 
         Class<?> clas = o.getClass();
@@ -18,26 +18,18 @@ public class Saver {
 
         Element element = clas.getAnnotation(Element.class);
         StringBuilder xml = new StringBuilder();
-        appendIndentation(xml, depth);
+        addIndent(xml, jumps);
         xml.append(String.format("<%s", element.name()));
 
         // Handling fields and properties
-        Map<String, String> fieldsXml = new HashMap<>();
         for (Method method : clas.getDeclaredMethods()) {
             if (method.isAnnotationPresent(ElementField.class)) {
                 ElementField field = method.getAnnotation(ElementField.class);
                 Object value = method.invoke(o);
-                fieldsXml.put(field.name(), value.toString());
+                xml.append(String.format(" %s=\"%s\"", field.name(), value.toString()));
             }
         }
 
-        // Append attributes or single value content to XML
-        boolean hasFields = !fieldsXml.isEmpty();
-        if (hasFields) {
-            for (Map.Entry<String, String> entry : fieldsXml.entrySet()) {
-                xml.append(String.format(" %s=\"%s\"", entry.getKey(), entry.getValue()));
-            }
-        }
         xml.append(">\n");
 
         // Handling sub-elements
@@ -47,29 +39,29 @@ public class Saver {
                 hasSubelements = true;
                 SubElements sub = method.getAnnotation(SubElements.class);
                 Object[] subObjects = (Object[]) method.invoke(o);
-                appendIndentation(xml, depth + 1);
+                addIndent(xml, jumps + 1);
                 xml.append(String.format("<%s>\n", sub.name()));
                 if (subObjects != null) {
                     for (Object subObj : subObjects) {
-                        xml.append(save(subObj, depth + 2)); // Recursive call for nested objects with increased depth
+                        xml.append(save(subObj, jumps + 2)); // Recursive call for nested objects with increased jumps
                     }
                 }
-                appendIndentation(xml, depth + 1);
+                addIndent(xml, jumps + 1);
                 xml.append(String.format("</%s>\n", sub.name()));
             }
         }
 
-        if (!hasSubelements && !hasFields) {
+        if (!hasSubelements) {
             xml.append("\n");
         }
-        appendIndentation(xml, depth);
+        addIndent(xml, jumps);
         xml.append(String.format("</%s>\n", element.name()));
 
         return xml.toString();
     }
 
-    private void appendIndentation(StringBuilder xml, int depth) {
-        for (int i = 0; i < depth; i++) {
+    private void addIndent(StringBuilder xml, int jumps) {
+        for (int i = 0; i < jumps; i++) {
             xml.append("  ");
         }
     }
